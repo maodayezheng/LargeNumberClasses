3a40a069465c79ba52a009f6170904592032c0bf
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-03 16:25:53 +0100

    add numerical issue of None

--- ModelUtils/Estimator/ImportanceEstimator.py
+++ ModelUtils/Estimator/ImportanceEstimator.py
@@ -24,9 +24,9 @@ class ImportanceEstimator(Estimator):
         target_scores = tf.reduce_sum(x * h, 1)
         self.target_exp_ = tf.exp(target_scores)
         # N x K
-        samples_scores = tf.matmul(h, samples, transpose_b=True)
+        samples_scores = tf.check_numerics(tf.matmul(h, samples, transpose_b=True), message="The sample score is")
         # N
-        self.Z_ =tf.check_numerics(tf.reduce_sum(tf.exp(samples_scores) / weights, 1), message="The Z is ")
+        self.Z_ = tf.check_numerics(tf.reduce_sum(tf.exp(samples_scores) / weights, 1), message="The Z is ")
 
         # The loss of each element in target
         # N
