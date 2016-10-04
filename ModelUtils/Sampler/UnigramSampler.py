eb49c8d0a50bbf71d4ee9178bb45353f62d0cca4
Botev <botevmg@gmail.com>
2016-10-04 13:53:33 +0100

    Added a few comments

--- ModelUtils/Sampler/UnigramSampler.py
+++ ModelUtils/Sampler/UnigramSampler.py
@@ -11,12 +11,13 @@ class UnigramSampler(Sampler):
             @Param num_targets: The length of target words or batch
         """
         targets = tf.reshape(targets, (-1, 1))
+        # Sample one class less because of the 0th padding token and shift the indices +1
         ss, _, _ = tf.nn.fixed_unigram_candidate_sampler(targets,
                                                          num_targets, self.num_samples_, self.unique_,
-                                                         self.num_classes_, unigrams=self.proposed_dist_[1:], distortion=
-                                                         self.distortion_)
+                                                         self.num_classes_ - 1, unigrams=self.proposed_dist_[1:],
+                                                         distortion=self.distortion_) + 1
 
-        tc = tf.nn.embedding_lookup(self.freq_embedding, targets + 1)
+        tc = tf.nn.embedding_lookup(self.freq_embedding, targets)
         tc = tf.squeeze(tf.pow(tc, self.distortion_))
-        sc = tf.squeeze(tf.pow(tf.nn.embedding_lookup(self.freq_embedding, ss + 1), self.distortion_))
+        sc = tf.squeeze(tf.pow(tf.nn.embedding_lookup(self.freq_embedding, ss), self.distortion_))
         return ss, tc, sc
