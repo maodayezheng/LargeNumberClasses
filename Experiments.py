4baa20489984447055cbb3c8851a847e31bd1a3d
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-04 10:25:25 +0100

    Change the default save path of variables

--- Experiments.py
+++ Experiments.py
@@ -77,7 +77,7 @@ def predict_next_word(params):
         raise Exception("{} type estimator is not support".format(estimator_type))
 
     # Initialise the word embedding layer
-    word_embedding = EmbeddingLayer(num_classes, input_dim, "word")
+    word_embedding = EmbeddingLayer(num_classes, input_dim, sampler_type+estimator_type+"word")
 
     # Reshape the input sentences
     sentences = tf.squeeze(tf.pack(inputs))
@@ -86,7 +86,7 @@ def predict_next_word(params):
     l = len(sentences)
 
     # Initialise Recurrent network
-    cell = GRU(input_dim, hidden_dim, output_dim, "next-word")
+    cell = GRU(input_dim, hidden_dim, output_dim, sampler_type+estimator_type)
     init_state = tf.zeros([batch_size, hidden_dim], dtype=tf.float32, name="init_state")
     state = init_state
     embedding = word_embedding.get_embedding()
