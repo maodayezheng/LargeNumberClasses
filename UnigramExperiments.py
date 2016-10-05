b2acd11cbfa1e2cec1904b9cea8b08d8b3c687d8
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-05 20:26:26 +0100

    0.75 ALEX and NEG

--- UnigramExperiments.py
+++ UnigramExperiments.py
@@ -4,6 +4,7 @@ import PredictNextWord
 
 def main():
     print("Dealing with Large number unigram test")
+    """
     estimator_types = ["IMP", "BLA", "NEG", "BER", "ALEX"]
     params = {"sampler_type": "unigram", "sample_size": 250,
               "batch_size": 50,
@@ -12,8 +13,13 @@ def main():
               "lamb": 0.001, "l_rate": 0.02, 'distortion': 1.0}
     for e in estimator_types:
         params["estimator_type"] = e
-
-    estimator_types = ["IMP", "BLA", "BER"]
+    """
+    params = {"sampler_type": "unigram", "sample_size": 250,
+              "batch_size": 50,
+              "sentence_len": 70, "epoch_step": 100, "input_dim": 100, "hidden_dim": 100,
+              "output_dim": 100,
+              "lamb": 0.001, "l_rate": 0.02, 'distortion': 1.0}
+    estimator_types = ["ALEX", "NEG"]
     params['distortion'] = 0.75
 
     for e in estimator_types:
