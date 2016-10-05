4e6b1c717dac62b3974e42eec109c395aa4231ba
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-05 17:34:42 +0100

    check Z without clip

--- ModelUtils/Estimator/Estimator.py
+++ ModelUtils/Estimator/Estimator.py
@@ -41,9 +41,9 @@ class Estimator(object):
         if self.target_score_ is None:
             self.target_score_ = tf.reduce_sum(x * h, 1)
         samples_scores = tf.matmul(h, embedding, transpose_b=True)
-        target_score = self.target_score_
+        target_score = tf.Print(self.target_score_, [tf.reduce_max(self.target_score_)], "The target is")
         samples_scores = tf.Print(samples_scores, [tf.reduce_min(samples_scores),
-                                                 tf.reduce_max(samples_scores)], "The sample score")
+                                                   tf.reduce_max(samples_scores)], "The sample score")
         # target_score, samples_scores = self.clip_likelihood(self.target_score_, samples_scores)
         Z = tf.reduce_sum(tf.exp(samples_scores), 1)
         Z = tf.Print(Z, [tf.reduce_max(Z), tf.reduce_min(Z)], "The value of Z is")
