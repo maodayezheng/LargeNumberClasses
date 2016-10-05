561dee2240d4c04237ffd67a0bd08648c92c429c
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-05 11:48:52 +0100

    test numpy save

--- UnigramExperiments.py
+++ UnigramExperiments.py
@@ -155,7 +155,7 @@ def predict_next_word(params):
     loss_check = iteration + epoch_step
     average_loss = 0
     exact_log_like_save = []
-    while iteration < 101:
+    while iteration < 300:
         iteration += 1
 
         # Randomly pick a data point from batch
