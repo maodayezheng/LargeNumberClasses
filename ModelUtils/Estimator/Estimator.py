a1cb755afce7e9a7625fa09d51768f4835f23bd1
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-05 19:22:42 +0100

    check IMP 0.0

--- ModelUtils/Estimator/Estimator.py
+++ ModelUtils/Estimator/Estimator.py
@@ -51,7 +51,7 @@ class Estimator(object):
                                   "The clipped ts")
         checker = tf.reduce_sum(tf.cast(tf.greater_equal(tf.reduce_max(samples_scores, 1) - target_score, 0), tf.int32))
         checker = tf.Print(checker,[checker], "The value of checker is")
-        Z = tf.reduce_sum(tf.exp(samples_scores), 1)
+        Z = tf.reduce_sum(tf.exp(samples_scores), 1) + 0*checker
         Z = tf.Print(Z, [tf.reduce_max(Z), tf.reduce_min(Z)], "The value of Z is")
         log_like = tf.reduce_mean((target_score - tf.log(Z + 1e-9)))
         return log_like
