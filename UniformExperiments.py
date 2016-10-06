394af1861be681383759729bbbcd641a7a20684b
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-06 13:38:50 +0100

    check ALEX 0.75 again

--- UniformExperiments.py
+++ UniformExperiments.py
@@ -4,20 +4,15 @@ import PredictNextWord
 
 def main():
     print("Dealing with Large number unigram test")
-    estimator_types = ["IMP"]
+   #estimator_types = ["IMP"]
     params = {"sampler_type": "unigram", "sample_size": 250,
               "batch_size": 50,
               "sentence_len": 70, "epoch_step": 100, "input_dim": 100, "hidden_dim": 100,
               "output_dim": 100,
               "lamb": 0.001, "l_rate": 0.02, 'distortion': 0.0}
 
-<<<<<<< Updated upstream
-    #estimator_types = ["ALEX", "NEG"]
-    #params["distortion"] = 0.75
-=======
-    estimator_types = ["ALEX", "BER"]
+    estimator_types = ["ALEX"]
     params["distortion"] = 0.75
->>>>>>> Stashed changes
 
     for e in estimator_types:
         params["estimator_type"] = e
