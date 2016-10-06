6248e9ea3f9a27dd0a064d6e1b0040183f3a08c2
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-06 15:13:45 +0100

    add IMP test

---
+++ IMP_TEST.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import PredictNextWord
+
+
+def main():
+    print("Dealing with Large number unigram test")
+    params = {"sampler_type": "unigram", "sample_size": 250,
+              "batch_size": 50,
+              "sentence_len": 70, "epoch_step": 100, "input_dim": 100, "hidden_dim": 100,
+              "output_dim": 100,
+              "lamb": 0.001, "l_rate": 0.02, 'distortion': 0.0, "estimator_type": "IMP"}
+
+    PredictNextWord.training(params)
+
+
+main()
