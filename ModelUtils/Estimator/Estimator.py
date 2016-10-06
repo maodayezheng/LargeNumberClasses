4c796a8e193b7ffc5a76f34f219b355b1db3a9a3
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-06 17:09:30 +0100

    Change the exact Log like approach

--- ModelUtils/Estimator/Estimator.py
+++ ModelUtils/Estimator/Estimator.py
@@ -38,8 +38,9 @@ class Estimator(object):
             @Return log_like: The exact log likelihood average over words
         """
 
-        samples_scores = tf.squeeze(tf.matmul(h, embedding, transpose_b=True))
-        target_score = tf.diag_part(tf.nn.embedding_lookup(samples_scores, x-1))
+        samples_scores = tf.transpose(tf.squeeze(tf.matmul(h, embedding, transpose_b=True)))
+        target_score = tf.squeeze(tf.nn.embedding_lookup(samples_scores, x-1))
+        target_score = tf.diag_part(tf.Print(target_score, [tf.shape(target_score)], "Target score"))
         Z = tf.reduce_sum(tf.exp(samples_scores), 1)
         log_like = tf.reduce_mean((target_score - tf.log(Z)))
         return log_like
