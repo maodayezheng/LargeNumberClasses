9c231ec95afaec9138855a52bdc51e8216fe8161
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-11 15:47:19 +0100

    smaller saving step

--- UniformExperiments.py
+++ UniformExperiments.py
@@ -9,7 +9,7 @@ def main():
               "batch_size": 20,
               "sentence_len": 70, "epoch": 10, "input_dim": 100, "hidden_dim": 100,
               "output_dim": 100,
-              "lamb": 0.001, "l_rate": 0.02, "save_step": 15000}
+              "lamb": 0.001, "l_rate": 0.02, "save_step": 2000}
     distortions = [0.0, 0.75, 1.0]
     for e in estimator_types:
         for d in distortions:
