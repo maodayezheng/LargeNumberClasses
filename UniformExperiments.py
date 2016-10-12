7f6cb9c955c0bdb5ad6b08094d4b6233c3cada4f
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2016-10-12 15:34:25 +0100

    Re run Alex 1.0 with 20 batch size

--- UniformExperiments.py
+++ UniformExperiments.py
@@ -6,7 +6,7 @@ def main():
     print("Dealing with Large number unigram test")
     estimator_types = ["ALEX"]
     params = {"sampler_type": "unigram", "sample_size": 250,
-              "batch_size": 50,
+              "batch_size": 20,
               "sentence_len": 70, "epoch": 10, "input_dim": 100, "hidden_dim": 100,
               "output_dim": 100,
               "lamb": 0.001, "l_rate": 0.02, "save_step": 2000}
@@ -16,5 +16,4 @@ def main():
             params["estimator_type"] = e
             params["distortion"] = d
             PredictNextWord.training(params)
-
 main()
